5600ed4772aef87cfb6b6f5e06a832e30a0671d8
Dmitriy.Olegovych <Dmeetriyolegovich@gmail.com>
2024-03-26 07:46:34 +0200

    tips added

--- reactStudy1/src/App.jsx
+++ reactStudy1/src/App.jsx
@@ -12,3 +12,8 @@ function App() {
 }
 
 export default App
+// !--git status -Check changed files-->
+// <!--git add . -Add all files in commit-->
+// <!--git commit -m "commit description" -To do commit-->
+// <!--git push origin main -Push changes to remote repository -->
+// <!--git pull origin main -Get fresh version from remote repository-->
